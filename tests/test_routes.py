564241ae03d45a4885966a644c4e1b53c1987117
DelGuidiceChristina <cthoma15@my.hpu.edu>
2023-03-03 12:19:55 -0500

    Added security headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -15,6 +15,7 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
+from service import talisman
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -22,6 +23,7 @@ DATABASE_URI = os.getenv(
 
 BASE_URL = "/accounts"
 
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 ######################################################################
 #  T E S T   C A S E S
@@ -37,6 +39,7 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
@@ -144,62 +147,17 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)   
 
     # TEST_LIST_ALL_ACCOUNTS
-    import service
-    from unittest.mock import patch, MagicMock
-    from service.models import Account
-    from service.routes import app
-    
-class TestListAccounts(unittest.TestCase):
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
 
-    def setUp(self):
-        app.config['TESTING'] = True
-        self.app = app.test_client()
-
-    def tearDown(self):
-        pass
-
-    @patch('service.models.Account.all')
-    def test_list_all_accounts(self, mock_all):
-        """Test that all accounts are returned"""
-        # Create mock accounts to return
-        mock_accounts = [
-            {'id': 1, 'name': 'John', 'email': 'john@example.com'},
-            {'id': 2, 'name': 'Jane', 'email': 'jane@example.com'}
-        ]
-        mock_all.return_value = [Account(**a) for a in mock_accounts]
-
-        # Send GET request to list all accounts
-        response = self.app.get('/accounts')
-
-        # Check that response code is HTTP_200_OK
-        self.assertEqual(response.status_code, 200)
-
-        # Check that the response data is a list of dictionaries
-        data = response.get_json()
-        self.assertIsInstance(data, list)
-        self.assertIsInstance(data[0], dict)
-
-        # Check that the response data matches the mock accounts
-        self.assertEqual(data, mock_accounts)
-
-    @patch('service.models.Account.all')
-    def test_list_all_accounts_empty(self, mock_all):
-        """Test that an empty list is returned when there are no accounts"""
-        mock_all.return_value = []
-
-        # Send GET request to list all accounts
-        response = self.app.get('/accounts')
-
-        # Check that response code is HTTP_200_OK
-        self.assertEqual(response.status_code, 200)
-
-        # Check that the response data is an empty list
-        data = response.get_json()
-        self.assertEqual(data, [])
-    
     # TEST_UPDATE_ACCOUNTS
-        def test_update_account(self):
-            """It should Update an existing Account"""
+    def test_update_account(self):
+        """It should Update an existing Account"""
         # create an Account to update
         test_account = AccountFactory()
         resp = self.client.post(BASE_URL, json=test_account.serialize())
@@ -219,3 +177,18 @@ class TestListAccounts(unittest.TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+    # TEST_FOR_SECURITY
+        def test_security_headers(self):
+            """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-XSS-Protection': '1; mode=block',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
